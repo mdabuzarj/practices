396dee88e326210e6c62854dbb44f2d85b806785
mdabuzarj <mohammed.abuzar.j54@gmail.com>
2026-01-20 23:35:48 +0530

    Sumof odd

---
+++ SumOfOddNumbers.java
@@ -0,0 +1,21 @@
+public class SumOfOddNumbers {
+
+    // function to calculate sum of odd numbers
+    static int oddSum(int n) {
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                sum = sum + i;
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+
+        int result = oddSum(n);
+        System.out.println(result);
+    }
+}
